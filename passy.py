ea11df56481f285eb203bc2d7a9d5b128d8bc001
alacrity <ameliatmorgana@proton.me>
2026-05-12 12:20:10 -0400

    Refractored in passy_gui to separate out into discrete functions

--- passy.py
+++ passy.py
@@ -1,62 +1,69 @@
-import secrets
-
-word_list = []
-passphrase = ""
-words_n = 4
-
-allowed_symbols = input("Enter valid special characters (default: !@#$%^&*-, type 'n' for none): ").lower()
-if allowed_symbols == "":
-    allowed_symbols = "!@#$%^&*-"
-elif allowed_symbols == "n" or allowed_symbols == "none":
-    allowed_symbols = ""
-
-if allowed_symbols != "":
-    sep_choice = input("Use only one symbol as a separator? (y/N): ")
-else:
-    sep_choice = ""
-
-use_one_symbol = sep_choice.lower()
-
-use_randome_number = input("Use randome number? (y/N): ").lower()
-
-with open("eff_words.txt") as file:
-    for line in file:
-        word_list.append(line.split()[1])
-
-selected_words = []
-
-for word in range(words_n):
-    new_word = secrets.choice(word_list)
-    selected_words.append(new_word)
-
-
-# pick a random index (0,1,2 or 3)
-caps_index = secrets.randbelow(words_n)
-selected_words[caps_index] = selected_words[caps_index].upper()
-
-# pick a random index to append a 0-9 to it
-
-if use_randome_number == "y":
-    num_index = secrets.randbelow(words_n)
-    num_random = secrets.randbelow(10)
-    selected_words[num_index] = selected_words[num_index] + str(num_random)
-
-if allowed_symbols:
-    if use_one_symbol == "y":
-        sep = secrets.choice(allowed_symbols)
-        for i, word in enumerate(selected_words):
-            passphrase += word
-            # Add the separator ONLY if this isn't the digit word
-            # AND it's not the very last word
-            if i != num_index and i < words_n - 1:
-                passphrase += sep
-    else:
-        for i, word in enumerate(selected_words):
-            passphrase += word
-            if i != num_index and i < words_n - 1:
-                passphrase += secrets.choice(allowed_symbols)
-else:
-    # If the user chose 'none', just squish them together
-    passphrase = "".join(selected_words)
-print(passphrase)
-
+# import secrets
+#
+# def load_words():
+#     # open the file and create the list
+#     word_list = []
+#     with open("eff_words.txt") as file:
+#         for line in file: word_list.append(line.split()[1])
+#     return
+#
+#
+# passphrase = ""
+# words_n = 4
+#
+# allowed_symbols = input("Enter valid special characters (default: !@#$%^&*-, type 'n' for none): ").lower()
+# if allowed_symbols == "":
+#     allowed_symbols = "!@#$%^&*-"
+# elif allowed_symbols == "n" or allowed_symbols == "none":
+#     allowed_symbols = ""
+#
+# if allowed_symbols != "":
+#     sep_choice = input("Use only one symbol as a separator? (y/N): ")
+# else:
+#     sep_choice = ""
+#
+# use_one_symbol = sep_choice.lower()
+#
+# use_randome_number = input("Use randome number? (y/N): ").lower()
+#
+# # with open("eff_words.txt") as file:
+# #     for line in file:
+# #         word_list.append(line.split()[1])
+#
+# selected_words = []
+#
+# for word in range(words_n):
+#     new_word = secrets.choice(word_list)
+#     selected_words.append(new_word)
+#
+#
+# # pick a random index (0,1,2 or 3)
+# caps_index = secrets.randbelow(words_n)
+# selected_words[caps_index] = selected_words[caps_index].upper()
+#
+# # pick a random index to append a 0-9 to it
+#
+# if use_randome_number == "y":
+#     num_index = secrets.randbelow(words_n)
+#     num_random = secrets.randbelow(10)
+#     selected_words[num_index] = selected_words[num_index] + str(num_random)
+#
+# if allowed_symbols:
+#     if use_one_symbol == "y":
+#         sep = secrets.choice(allowed_symbols)
+#         for i, word in enumerate(selected_words):
+#             passphrase += word
+#             # Add the separator ONLY if this isn't the digit word
+#             # AND it's not the very last word
+#             if i != num_index and i < words_n - 1:
+#                 passphrase += sep
+#     else:
+#         for i, word in enumerate(selected_words):
+#             passphrase += word
+#             if i != num_index and i < words_n - 1:
+#                 passphrase += secrets.choice(allowed_symbols)
+# else:
+#     # If the user chose 'none', just squish them together
+#     passphrase = "".join(selected_words)
+# print(passphrase)
+#
---
+++ passy_gui.py
@@ -0,0 +1,53 @@
+import secrets
+
+def load_words(filename):
+    # open the file and create the list
+    words = []
+    with open(filename) as file:
+        for line in file: words.append(line.split()[1])
+    return words
+
+def select_random_words(word_pool, count):
+    # ask user for number of words
+    selected_words = []
+
+    for _ in range(words_num):
+        new_word = secrets.choice(word_list)
+        selected_words.append(new_word)
+    return selected_words
+
+def add_word_complexity(words_list,use_digit):
+    count = len(words_list)
+    # Capitalize a random word
+    caps_index = secrets.randbelow(count)
+    words_list[caps_index] = words_list[caps_index].upper()
+    if use_digit:
+        num_index = secrets.randbelow(count)
+        words_list[num_index] += str(secrets.randbelow(10))
+    return words_list
+
+def assemble_passphrase(words_list, symbols_used, use_single_symbol):
+    # Join word pool to generate a single passphrase
+    passphrase = ''
+    count = len(words_list)
+    # If the user just wants to use a single symbol, we'll randomly select 1 index from the length of symbols
+    if use_single_symbol:
+        sing_sym = symbols_used[secrets.randbelow(len(symbols_used))]
+    for i, word in enumerate(words_list):
+        passphrase += word
+        if i < (count - 1) and symbols_used:
+            if use_single_symbol:
+                passphrase += sing_sym
+            else:
+                passphrase += secrets.choice(symbols_used)
+    return passphrase
+
+filepath = "eff_words.txt"
+words_num = int(input("Number of words to use: "))
+allowed_symbols = input("Symbols: ")
+word_list = load_words(filepath)
+wordpool = select_random_words(word_list, words_num)
+wordpool = add_word_complexity(wordpool,True)
+passphrase = assemble_passphrase(wordpool,allowed_symbols,True)
+print(wordpool)
+print(passphrase)
